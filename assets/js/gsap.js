352bd7921d21ccb0d7a96bac3920ff68073aebc3
saran.baral <saranbrl35@gmail.com>
2024-10-02 13:32:08 +0545

    animation addeition

---
+++ assets/js/gsap.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#about-text", {
+  x: 450,
+  y: 100,
+  duration: 3,
+  scrollTrigger: {
+    trigger: "#about",
+    start: "-500px",
+    end: "top top",
+    scrub: 1,
+    // markers: true
+  },
+});
+gsap.to("#about-desc", {
+  x: -400,
+  y: -50,
+  duration: 3,
+  scrollTrigger: {
+    trigger: "#about",
+    scrub: 1,
+    start: "-500px",
+    end: "top top",
+    markers: true,
+  },
+});
