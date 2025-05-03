19972a99d59cc70013f5b6bb09e3343e92c6a5fe
Saran Baral (zirea) <saranbrl35@gmail.com>
2025-05-03 13:06:46 +0545

    updated anims

--- assets/js/projectScroll.js
+++ assets/js/projectScroll.js
@@ -4,29 +4,29 @@ function scrollProjectContent() {
   const _scrollContent = gsap.utils.toArray(".pro-container");
   let _mainWIdth = window.innerWidth
   let _containerScrollWidth = _scrollContainer.scrollWidth
-
-  function getScrollWidth() {
-    return ((_containerScrollWidth) - _mainWIdth) + 42
-  }
-  gsap.from(_scrollContent, {
-    x: getScrollWidth() * -1,
-    ease: "none",
-    scrollTrigger: {
-      trigger: "#section-four",
-      start: "top 5%",
-      pin: true,
-      scrub: true,
-      end: getScrollWidth() + 1000,
-      invalidateOnRefresh: true,
-      anticipatePin: 1,
-      markers: true
+  if (_mainWIdth > 960) {
+    function getScrollWidth() {
+      return ((_containerScrollWidth) - _mainWIdth) + 42
     }
-  })
+    gsap.from(_scrollContent, {
+      x: getScrollWidth() * -1,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#section-four",
+        start: "top 5%",
+        pin: true,
+        scrub: true,
+        end: getScrollWidth() + 1000,
+        invalidateOnRefresh: true,
+        anticipatePin: 1,
+        // markers: true
+      }
+    })
+  }
 }
-scrollProjectContent();
 
+scrollProjectContent();
 var onresize = function () {
   scrollProjectContent();
-
 }
 window.addEventListener("resize", onresize);
