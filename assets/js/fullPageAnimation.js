fb0ffca49f5445f101b83e5ba5b3c1fc53bec8c9
Saran Baral (zirea) <saranbrl35@gmail.com>
2025-05-23 14:58:34 +0545

    smooth anim added

--- assets/js/fullPageAnimation.js
+++ assets/js/fullPageAnimation.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
     smooth: 4,
-    smoothTouch: 0.1,
+    smoothTouch: 2,
     effects: true,
   });
 function animateWholePage() {
